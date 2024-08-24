81ac791541073aeb6560592d4810185bc1ca20ae
akda007 <akdagmr007s@gmail.com>
2024-08-24 00:37:29 -0300

    Changed sequelize config back to mssql

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const sequelize = require('sequelize');
 //configurações da base de dados
-const database = new sequelize('crud', 'root', '123321',
-// const database = new sequelize('crud', 'crud123', 'crud123456789',
+const database = new sequelize('crud', 'crud123', 'crud123456789', //Lembra de atualizar o username e password caso mude para MySQL
 {
-dialect: 'mysql', host:'localhost', port: 3306
+dialect: 'mssql', host:'localhost', port: 1433 //SQL SERVER
+// dialect: 'mysql', host:'localhost', port: 3306 //MY SQL
 });
 database.sync();
 module.exports = database;
